d35514ce6c0d48a6235066cd49dbb8f7839f2f07
Alessandro Midolo <alessandro141200@gmail.com>
2025-09-18 10:59:15 +0200

    Enable MSW for GitHub Pages

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,16 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./themes/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log("Mock mode on? ", process.env.REACT_APP_USE_MOCK);
+// console.log("Mock mode on? ", process.env.REACT_APP_USE_MOCK);
+
+if (process.env.REACT_APP_USE_MOCK === "true") {
+  const { worker } = require("./mocks/browser");
+  worker.start({
+    serviceWorker: {
+      url: `${process.env.PUBLIC_URL}/mockServiceWorker.js`,
+    },
+  });
+}
 
 root.render(
   <ThemeProvider theme={theme}>
